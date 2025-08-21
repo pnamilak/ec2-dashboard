e2c430592882d1d3d4220415ada30adc7bb76d5f
pnamilak <pranay.sqldba@gmail.com>
2025-08-21 18:43:03 +0530

    updated

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,16 @@
-# (already included at bottom of main.tf, keep either place)
+output "s3_website_url" {
+  value = "http://${aws_s3_bucket.frontend.bucket}.s3-website-${var.aws_region}.amazonaws.com"
+}
+
+output "api_gateway_url" {
+  value = aws_apigatewayv2_api.api.api_endpoint
+}
+
+output "lambda_handler_name" {
+  value = aws_lambda_function.ec2_handler.function_name
+}
+
+output "authorizer_lambda_name" {
+  value = aws_lambda_function.authorizer.function_name
+}
+
