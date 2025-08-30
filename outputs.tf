31c03252173bae0a23a7a071246f84627406501f
pnamilak <pranay.sqldba@gmail.com>
2025-08-30 22:37:10 +0530

    updated

--- outputs.tf
+++ outputs.tf
@@ -19,3 +19,23 @@ output "lambda_api_name" {
 output "lambda_authorizer_name" {
   value = aws_lambda_function.authorizer.function_name
 }
+
+output "site_bucket" {
+  description = "S3 bucket name serving the website"
+  value       = aws_s3_bucket.website.bucket
+}
+
+output "cloudfront_id" {
+  description = "CloudFront distribution ID"
+  value       = aws_cloudfront_distribution.site.id
+}
+
+output "cloudfront_domain" {
+  description = "CloudFront domain"
+  value       = aws_cloudfront_distribution.site.domain_name
+}
+
+output "api_base_url" {
+  description = "HTTP API base URL"
+  value       = aws_apigatewayv2_api.api.api_endpoint
+}
